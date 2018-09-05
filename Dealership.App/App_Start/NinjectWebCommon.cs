989feb97937fb60e4aa5fb15a38b9a8d5c708518
ennab <40575668+ennab@users.noreply.github.com>
2018-09-04 20:00:53 -0500

    AddingNinjectExtensionsConventions

--- Dealership.App/App_Start/NinjectWebCommon.cs
+++ Dealership.App/App_Start/NinjectWebCommon.cs
@@ -3,14 +3,13 @@
 
 namespace Dealership.App.App_Start
 {
-    using BLL;
-    using DAL;
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
     using Ninject;
+    using Ninject.Extensions.Conventions;
     using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
     using System;
     using System.Web;
-
     public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
@@ -46,6 +45,21 @@ namespace Dealership.App.App_Start
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
+                /*
+                kernel.Bind(x =>
+                {
+                    x.FromThisAssembly()
+                    .SelectAllClasses()
+                    .BindDefaultInterface();
+                });
+                */
+                kernel.Bind(x =>
+                {
+                    x.FromAssemblyContaining<DAL.UnitOfWork>()
+                  .SelectAllClasses()
+                  .BindDefaultInterface();
+                });
+
                 return kernel;
             }
             catch
@@ -61,7 +75,7 @@ namespace Dealership.App.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
+            //kernel.Bind<IUnitOfWork>().To<UnitOfWork>();
         }
     }
 }
